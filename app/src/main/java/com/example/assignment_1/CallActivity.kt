3b34c8675f5cd63b6f9cea58986e174f99f8d3c0
fizawajid <fizawajid001@gmail.com>
2025-09-12 15:51:30 +0500

    added the UI of stories, highlights, and more pictures in UI

---
+++ app/src/main/java/com/example/assignment_1/CallActivity.kt
@@ -0,0 +1,26 @@
+package com.example.assignment_1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+
+class CallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.call) // make sure login_config.xml exists
+
+
+//        val backbtn = findViewById< ImageButton>(R.id.back_arrow)
+//
+//        backbtn.setOnClickListener {
+//            val intent = Intent(this, MessagesActivity::class.java)
+//            startActivity(intent)
+//        }
+
+
+    }
+}
+
